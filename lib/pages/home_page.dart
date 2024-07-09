25a3538e8362835e00d20abf985d3f3e32a25b20
galal-mahmoud <galalengineer45@gmail.com>
2024-07-09 21:24:09 +0200

    create notes app

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,6 +5,12 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Notes App'
+        ),
+      ),
+    );
   }
 }
